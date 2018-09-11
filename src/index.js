37638e46f63f5a2149183508673d367cc65a51c8
Ubuntu <trihoang@bu.edu>
2018-09-11 14:03:52 -0400

    update to use store.js-moretypes plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import store from "store";
+var moreTypes = require("store.js-moretypes")
+store.addPlugin(moreTypes)
 
 export default class SimpleStorage extends Component {
   constructor(props) {
